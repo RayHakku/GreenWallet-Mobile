18e533e6386bf49daa102e579c3a83f04e50abfa
RayHakku <104810422+RayHakku@users.noreply.github>
2023-09-06 13:32:30 -0300

    add GetStartedScreen.kt and update gradle dependecy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.greenwallet"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.greenwallet"
@@ -56,6 +56,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.navigation:navigation-compose:2.4.0-alpha10")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/greenwallet/Screens/GetStartedScreen.kt
@@ -0,0 +1,113 @@
+package com.example.greenwallet.Screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.greenwallet.R
+
+@Preview
+@Composable
+fun GetStarted( modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.hsl(104F, 1F, 0.9F, 1f))
+            .padding(20.dp),
+        // verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    )
+    {
+        Image(
+            painter = painterResource(id = R.drawable.wallet_cuate2),
+            contentDescription = "Woman with a wallet",
+            modifier = Modifier
+                .padding(vertical = 40.dp)
+        )
+        Surface(
+            modifier = Modifier
+                .padding(vertical = 50.dp)
+                //.fillMaxHeight(0.35f)
+                .fillMaxWidth(),
+            shape = MaterialTheme.shapes.large,
+        )
+        {
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+            )
+            {
+                Text(
+                    text = "Comece Agora",
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center,
+                    lineHeight = 5.sp,
+                    fontSize = 32.sp,
+                    color = Color.Black,
+                    fontStyle = androidx.compose.ui.text.font.FontStyle.Normal,
+                    fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(bottom = 10.dp , top = 15.dp),
+                )
+                Text(
+                    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    textAlign = androidx.compose.ui.text.style.TextAlign.Center,
+                    fontSize = 14.sp,
+                    color = Color.hsl(0F, 0F,0F,0.3f),
+                    lineHeight = 18.sp,
+                    modifier = Modifier
+                        .padding(bottom = 20.dp)
+                        .fillMaxWidth(0.8f),
+
+
+                    )
+                Button(
+                    onClick = { /*TODO*/ },
+                    shape = MaterialTheme.shapes.small,
+                    modifier = Modifier
+                        .fillMaxWidth(0.6f)
+                        .padding(bottom = 5.dp),
+                    colors = androidx.compose.material3.ButtonDefaults.buttonColors(
+                        containerColor = Color.hsl(104F, 0.62F, 0.51F, 1f),
+                        contentColor = Color.White,
+                    ),
+                    border = androidx.compose.material3.ButtonDefaults.outlinedButtonBorder
+
+                ) {
+                    Text(
+                        text = "Criar Conta"
+                    )
+                }
+                Button(
+                    onClick = { /*TODO*/ },
+                    shape = MaterialTheme.shapes.small,
+                    colors = androidx.compose.material3.ButtonDefaults.buttonColors(
+                        containerColor = Color.hsl(104F, 0.62F, 0.51F, 0.1f),
+                        contentColor = Color.hsl(104F, 0.62F, 0.51F, 1f),
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth(0.6f)
+                        .padding(top = 10.dp, bottom = 30.dp),
+                    border = androidx.compose.material3.ButtonDefaults.outlinedButtonBorder
+
+                ) {
+                    Text(
+                        text = "Entrar",
+                    )
+                }
+            }
+        }
+    }
+}
