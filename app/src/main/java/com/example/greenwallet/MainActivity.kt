603067d51a432acac3192d39c3e282dbf52e432b
RayHakku <104810422+RayHakku@users.noreply.github>
2023-09-06 06:57:30 -0300

    Activate Relay Plugin and add Some Ui Packages

--- app/src/main/java/com/example/greenwallet/MainActivity.kt
+++ app/src/main/java/com/example/greenwallet/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.greenwallet.ui.theme.GreenWalletTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +23,7 @@ class MainActivity : ComponentActivity() {
             GreenWalletTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Rayan")
                 }
             }
         }
@@ -28,16 +32,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+    Surface (color = Color.Blue) {
+        Text(
+            text = "Hi, my name is $name!",
+            color = Color.White,
+            fontSize = 12.sp,
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GreenWalletTheme {
-        Greeting("Android")
+        Greeting("Rayan")
     }
 }
